8738527d2f9b8f3d6cbe1012bade4efc66b54c0b
James Hartman <jameshartman@Jamess-MacBook-Pro.local>
2017-12-31 21:37:49 -0500

    added objects functions

--- js/src/optional/objects.js
+++ js/src/optional/objects.js
@@ -1,11 +1,17 @@
 alterContext = (fn, obj) => {
-  // Optional Challenge
+  return fn.call(obj);
 };
 
 alterObjects = (constructor, greeting) => {
-  // Optional Challenge
+  constructor.prototype.greeting = greeting;
+  return constructor.greeting;
 };
 
 iterate = (obj) => {
   // Optional Challenge
+  properties = []
+  for(const [key, value] of Object.entries(obj)){
+    properties.push(`${key}: ${value}`)
+  }
+  return properties
 };
